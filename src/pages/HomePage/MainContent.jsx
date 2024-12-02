0cedac409aed3c9f095faafaff8f0809f6c6a32c
Ajit Singh Parihar <ajitsinghprhr123@gmail.com>
2024-12-02 16:19:05 +0530

    HomePage Modifications

--- src/pages/HomePage/MainContent.jsx
+++ src/pages/HomePage/MainContent.jsx
@@ -2,23 +2,51 @@ import React from 'react';
 import EducationChallenges from './EducationalChallenges';
 import RevolutionarySystem from './RevolutionarySystem';
 import PersonalizedLearning from './PersonalizedLearning';
+import { GalleryImage } from "./GalleryImage";
+const galleryImages = [
+  {
+    src: "https://cdn.builder.io/api/v1/image/assets%2F170cbe9c02a2485986a6dc949bdc8ad3%2F6ee99432fa6540c3806389414f37bb46",
+    className: "mt-16 ml-32 w-[70%] max-sm:mt-7",
+    aspectRatio: "1.45"
+  },
+  {
+    src: "https://cdn.builder.io/api/v1/image/assets%2F170cbe9c02a2485986a6dc949bdc8ad3%2Fa9ec8e1e378c4e8db1facd802f1ad428",
+    className: "mt-5 w-full",
+    aspectRatio: "1.4"
+  },
+  {
+    src: "https://cdn.builder.io/api/v1/image/assets%2F170cbe9c02a2485986a6dc949bdc8ad3%2F849f2d2280a04b038422118c4a7e0dbe",
+    className: "mt-16 ml-0 w-[70%]",
+    aspectRatio: "1.45"
+  }
+];
 
 function MainContent() {
+  
   return (
     <main>
-      <section className="min-h-screen flex flex-col items-center justify-center bg-indigo-50 py-16">
+      <section className="min-h-screen flex flex-col items-center justify-center bg-white-50 py-16">
         <EducationChallenges />
       </section>
 
-      <section className="min-h-screen flex flex-col items-center justify-center bg-white py-10">
-        <h2 className="text-3xl font-bold text-center text-amber-500 mb-16 px-4 max-w-4xl">
+      <section className="min-h-screen flex flex-col items-center justify-center bg-white py-16 mt-[-100px]">
+        <h2 className="text-3xl font-bold text-center text-amber-500 mb-8 px-4 max-w-4xl">
           Redefining education to unlock potential and create brighter futures for every student.
         </h2>
-        <img
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/3e1ee0c20767fb35fc2a7ed3a382cf408cb1d0424619595a661a560fc3f15ee1?placeholderIfAbsent=true&apiKey=170cbe9c02a2485986a6dc949bdc8ad3"
-          alt="Educational concept illustration"
-          className="w-full max-w-6xl rounded-xl shadow-lg"
-        />
+        <div>
+      <div className="flex gap-5 max-md:flex-col">
+        {galleryImages.map((image, index) => (
+          <div key={index} className="flex flex-col w-[33%] max-md:ml-0 max-md:w-full">
+            <GalleryImage
+              src={image.src}
+              className={image.className}
+              aspectRatio={image.aspectRatio}
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
+    </div>
       </section>
 
       <RevolutionarySystem />
