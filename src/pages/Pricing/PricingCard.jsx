1c572ccbce5b060dac49404a3e00f9d5f6a8974c
Ajit Singh Parihar <ajitsinghprhr123@gmail.com>
2024-12-26 12:01:46 +0530

    Check 61

--- src/pages/Pricing/PricingCard.jsx
+++ src/pages/Pricing/PricingCard.jsx
@@ -1,6 +1,12 @@
 import * as React from "react";
-
-const PricingCard = ({ title, price, recommended, bestValue, buttonColor, borderColor, features }) => {
+import { useState } from "react";
+const PricingCard = ({ title, price, recommended, bestValue, buttonColor, borderColor, features,Prerequisite,featuresDescription }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  
+    const toggleDescription = () => {
+      setIsOpen(!isOpen);
+    };
+  
   return (
     <div className={`flex flex-col font-inter rounded-xl w-[30%] max-md:w-full ${recommended ? 'bg-sky-500' : bestValue ? 'bg-green-600' : ''} p-2`}>
       <div
@@ -17,11 +23,17 @@ const PricingCard = ({ title, price, recommended, bestValue, buttonColor, border
         <div className="self-center text-lg">{title}</div>
         <div className="self-center mt-2 text-xl text-sky-950">
           <span className="text-sky-950">₹ {price}/</span>
-          <span>User</span>
+          <span>Year</span>
         </div>
-        <div className="self-center mt-2 text-sm text-zinc-600">Per Month</div>
+        <div className="self-center mt-2 text-sm text-zinc-600">Annual</div>
         <div className="self-center mt-2 text-xs text-zinc-600">(Includes 1000 Users)</div>
         <div className="my-4 w-full h-px border border-gray-300" />
+        {/* Render prerequisite if it exists */}
+        {Prerequisite && (
+          <div className="self-center mb-2 text-sm text-blue-700">
+            <strong> {Prerequisite} </strong>
+          </div>
+        )}
         {features?.map((feature, index) => (
           <div key={index} className="flex gap-2 items-center mt-2">
             <img
@@ -29,15 +41,27 @@ const PricingCard = ({ title, price, recommended, bestValue, buttonColor, border
               src="https://cdn.builder.io/api/v1/image/assets/TEMP/6584c070b022da334e18774febceaeef0a5298e7feeb409d64c5e4c9276cf2da?placeholderIfAbsent=true&apiKey=af5cfb29bb594d4e9f9e505ea3916323"
               alt=""
               className="w-4 h-4"
+              onClick={toggleDescription}
             />
             <div className="text-sm">{feature}</div>
+            {/* <div
+                className={`${
+                  isOpen ? 'max-h-40' : 'max-h-0'
+                } overflow-hidden transition-all duration-300 ease-in-out mt-2`} 
+                style={{ marginTop: isOpen ? "0px" : "0" }} 
+              >
+                <p className={`text-xs `}>{featuresDescription}</p>
+           </div> */}
           </div>
         ))}
-        <button
-          className={`overflow-hidden px-6 py-2 mt-6 rounded-lg border-2 border-solid text-${buttonColor} border-${borderColor}`}
+         <button
+          className={`overflow-hidden px-6 py-2 mt-6 rounded-lg border-2 border-solid text-${buttonColor} border-${borderColor} relative group`}
           tabIndex={0}
         >
-          Get Started
+          <span className="relative z-10 group-hover:text-white">Get Started</span>
+          <span
+            className={`absolute top-0 left-0 w-0 h-full bg-${borderColor} font-inter transition-all duration-300 group-hover:w-full`}
+          ></span>
         </button>
       </div>
     </div>
--- src/pages/Pricing/PricingSection.jsx
+++ src/pages/Pricing/PricingSection.jsx
@@ -3,33 +3,341 @@ import Footer from "../Footer/Footer";
 import * as React from "react";
 import PricingCard from "./PricingCard";
 
-const pricingData = [
+const pricingData = {
+  ERP: [
   {
-    title: "Essential Package",
-    price: "20",
-    features: ["Student Management", "Student Management", "Student Management", "Student Management"],
+    title: "Basic ERP",
+    price: "25000",
+    features: [
+      "Student Management",
+      "Attendance",
+      "Fee Management",
+      "Timetable",
+      "Basic Reports"
+    ],
+    featuresDescription: [
+      "Add/edit student profiles, ID generation",
+      "Daily/period tracking",
+      "Invoice generation, payment tracking",
+      "Automated class scheduling",
+      "Exportable data for attendance, fees, and schedules"
+    ],
     buttonColor: "sky-500",
     borderColor: "sky-500"
   },
   {
-    title: "Essential Package",
-    price: "20",
+    title: "Professional ERP",
+    price: "50000",
     recommended: true,
-    features: ["Student Management", "Student Management", "Student Management", "Student Management"],
+    Prerequisite: "Basic ERP+",
+    features: [
+      // "Student Management",
+      // "Attendance",
+      // "Fee Management",
+      // "Timetable",
+      // "Basic Reports",
+      "Exam Management",
+      "Inventory Management",
+      "Campus Management",
+      "Library Management"
+    ],
+    featuresDescription: [
+      "Add/edit student profiles, ID generation",
+      "Daily/period tracking",
+      "Invoice generation, payment tracking",
+      "Automated class scheduling",
+      "Exportable data for attendance, fees, and schedules",
+      "Grading, results, report cards",
+      "Track assets and resources",
+      "Manage facilities, transport, and extracurriculars",
+      "Catalog books, issue/return tracking"
+    ],
     buttonColor: "sky-500",
     borderColor: "sky-500"
   },
   {
-    title: "Essential Package",
-    price: "20",
+    title: "Enterprise ERP",
+    price: "75000",
     bestValue: true,
-    features: ["Student Management", "Student Management", "Student Management", "Student Management"],
+    Prerequisite: "Professional ERP+",
+    features: [
+      // "Student Management",
+      // "Attendance",
+      // "Fee Management",
+      // "Timetable",
+      // "Basic Reports",
+      // "Exam Management",
+      // "Inventory Management",
+      // "Campus Management",
+      // "Library Management",
+      "Advanced Analytics",
+      "Mobile App Access",
+      "Admissions",
+      "Hostel Management"
+    ],
+    featuresDescription: [
+      "Add/edit student profiles, ID generation",
+      "Daily/period tracking",
+      "Invoice generation, payment tracking",
+      "Automated class scheduling",
+      "Exportable data for attendance, fees, and schedules",
+      "Grading, results, report cards",
+      "Track assets and resources",
+      "Manage facilities, transport, and extracurriculars",
+      "Catalog books, issue/return tracking",
+      "Dashboards, predictive trends",
+      "Parent and teacher portals",
+      "Online application, review, and approval",
+      "Room allocation, mess schedules, fee tracking"
+    ],
+    buttonColor: "green-600",
+    borderColor: "green-600"
+  },
+],
+LMS: [
+  {
+    title: "Starter LMS",
+    price: "20000",
+    features: [
+      "Course Creation",
+      "Assignment Management",
+      "Basic Reporting",
+      "Lesson Plans"
+    ],
+    featuresDescription: [
+      "Upload content, organize by topics",
+      "Online submissions, grading",
+      "Student performance and course completion",
+      "Generate NCERT-aligned plans"
+    ],
+    buttonColor: "sky-500",
+    borderColor: "sky-500"
+  },
+  {
+    title: "Advanced LMS",
+    price: "40000",
+    recommended: true,
+    Prerequisite: "Starter LMS+",
+    features: [
+      // "Course Creation",
+      // "Assignment Management",
+      // "Basic Reporting",
+      // "Lesson Plans",
+      "Discussion Forums",
+      "Gradebook",
+      "Quizzes",
+      "Homework Generator"
+    ],
+    featuresDescription: [
+      "Upload content, organize by topics",
+      "Online submissions, grading",
+      "Student performance and course completion",
+      "Generate NCERT-aligned plans",
+      "Peer-to-peer engagement",
+      "Automated grade tracking",
+      "Timed tests, instant results",
+      "Auto-generate assignments based on topics"
+    ],
+    buttonColor: "sky-500",
+    borderColor: "sky-500"
+  },
+  {
+    title: "Premium LMS",
+    price: "60000",
+    bestValue: true,
+    Prerequisite: "Advanced LMS+",
+    features: [
+      // "Course Creation",
+      // "Assignment Management",
+      // "Basic Reporting",
+      // "Lesson Plans",
+      // "Discussion Forums",
+      // "Gradebook",
+      // "Quizzes",
+      // "Homework Generator",
+      "Gamification",
+      "PAL (Personalized Adaptive Learning)",
+      "Mobile App Access",
+      "ERP Integration"
+    ],
+    featuresDescription: [
+      "Upload content, organize by topics",
+      "Online submissions, grading",
+      "Student performance and course completion",
+      "Generate NCERT-aligned plans",
+      "Peer-to-peer engagement",
+      "Automated grade tracking",
+      "Timed tests, instant results",
+      "Auto-generate assignments based on topics",
+      "Badges, leaderboards, and rewards",
+      "Customized learning paths",
+      "Learning on the go",
+      "Seamless connection with ERP modules"
+    ],
+    buttonColor: "green-600",
+    borderColor: "green-600"
+  },
+],
+CareerCounselling: [
+  {
+    title: "Awareness Module",
+    price: "15000",
+    features: [
+      "Career Exploration Tools",
+      "Interest & Personality Tests",
+      "Career Curriculum"
+    ],
+    featuresDescription: [
+      "Browse career paths",
+      "RIASEC-based profiling",
+      "Structured learning materials for career awareness"
+    ],
+    buttonColor: "sky-500",
+    borderColor: "sky-500"
+  },
+  {
+    title: "Counselling Module",
+    price: "30000",
+    recommended: true,
+    Prerequisite: "Awareness Module+",
+    features: [
+      // "Career Exploration Tools",
+      // "Interest & Personality Tests",
+      // "Career Curriculum",
+      "Personalized Career Counselling",
+      "Occupation Info",
+      "Education Pathways",
+      "Scholarship Details"
+    ],
+    featuresDescription: [
+      "Browse career paths",
+      "RIASEC-based profiling",
+      "Structured learning materials for career awareness",
+      "AI-driven insights",
+      "Job roles, skills required",
+      "Higher studies and professional options",
+      "Access to financial aid options"
+    ],
+    buttonColor: "sky-500",
+    borderColor: "sky-500"
+  },
+  {
+    title: "Comprehensive Module",
+    price: "50000",
+    bestValue: true,
+    Prerequisite: "Counselling Module+",
+    features: [
+      // "Career Exploration Tools",
+      // "Interest & Personality Tests",
+      // "Career Curriculum",
+      // "Personalized Career Counselling",
+      // "Occupation Info",
+      // "Education Pathways",
+      // "Scholarship Details",
+      "Career Preparation Tools",
+      "Competitive Exam Guidance",
+      "Self-Service Tools"
+    ],
+    featuresDescription: [
+      "Browse career paths",
+      "RIASEC-based profiling",
+      "Structured learning materials for career awareness",
+      "AI-driven insights",
+      "Job roles, skills required",
+      "Higher studies and professional options",
+      "Access to financial aid options",
+      "Mock interviews, CV/resume templates",
+      "Exam lists, preparation tips",
+      "Personalized dashboards for students"
+    ],
+    buttonColor: "green-600",
+    borderColor: "green-600"
+  },
+],
+HRMS: [
+  {
+    title: "Basic HRMS",
+    price: "30000",
+    features: [
+      "Staff Attendance",
+      "Payroll Management",
+      "Leave Tracking"
+    ],
+    featuresDescription: [
+      "Track daily attendance",
+      "Salary computation, tax deductions",
+      "Apply, approve, and manage leaves"
+    ],
+    buttonColor: "sky-500",
+    borderColor: "sky-500"
+  },
+  {
+    title: "Professional HRMS",
+    price: "50000",
+    recommended: true,
+    Prerequisite: "Basic HRMS+",
+    features: [
+      // "Staff Attendance",
+      // "Payroll Management",
+      // "Leave Tracking",
+      "Recruitment Module",
+      "Appraisals",
+      "HR Workflows"
+    ],
+    featuresDescription: [
+      "Track daily attendance",
+      "Salary computation, tax deductions",
+      "Apply, approve, and manage leaves",
+      "Post job openings, track applications",
+      "Evaluate employee performance",
+      "Automate routine HR tasks"
+    ],
+    buttonColor: "sky-500",
+    borderColor: "sky-500"
+  },
+  {
+    title: "Advanced HRMS",
+    price: "75000",
+    bestValue: true,
+    Prerequisite: "Professional HRMS+",
+    features: [
+      // "Staff Attendance",
+      // "Payroll Management",
+      // "Leave Tracking",
+      // "Recruitment Module",
+      // "Appraisals",
+      // "HR Workflows",
+      "Training Modules",
+      "Self-Service Portal",
+      "Mobile HR Management"
+    ],
+    featuresDescription: [
+      "Track daily attendance",
+      "Salary computation, tax deductions",
+      "Apply, approve, and manage leaves",
+      "Post job openings, track applications",
+      "Evaluate employee performance",
+      "Automate routine HR tasks",
+      "Plan and track staff training",
+      "Allow staff to manage their profiles and tasks",
+      "Access HR tools on mobile devices"
+    ],
     buttonColor: "green-600",
     borderColor: "green-600"
   }
-];
-
+]
+};
 function PricingSection() {
+  const [selectedCategory, setSelectedCategory] = React.useState("ERP");
+
+  const handleButtonClick = (category) => {
+    setSelectedCategory(category);
+  };
+  const buttonStyle = (category) => {
+    return selectedCategory === category
+      ? "bg-slate-700 text-amber-600 border-green-600"
+      : "bg-white text-indigo-950 border-green-600";
+  };
   return (
     <>
     <Header />
@@ -44,18 +352,42 @@ function PricingSection() {
             toward effortless management today.
           </div>
           <div className="flex overflow-hidden gap-10 px-10 py-2 mt-10 max-w-full font-inter text-md items-start justify-between font-medium bg-white border-2 border-green-600 border-solid rounded-[30px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)] text-indigo-950 w-[738px] max-md:px-5 max-md:mt-10">
-            <button tabIndex={0}>ERP</button>
-            <button tabIndex={0}>LMS</button>
-            <button tabIndex={0}>HRMS</button>
-            <button tabIndex={0} className="">Career Counselling</button>
+          <button
+                onClick={() => handleButtonClick("ERP")}
+                className={`py-2 px-4 rounded-full ${buttonStyle("ERP")}`}
+                tabIndex={0}
+              >
+                ERP
+              </button>
+              <button
+                onClick={() => handleButtonClick("LMS")}
+                className={`py-2 px-4 rounded-full ${buttonStyle("LMS")}`}
+                tabIndex={0}
+              >
+                LMS
+              </button>
+              <button
+                onClick={() => handleButtonClick("HRMS")}
+                className={`py-2 px-4 rounded-full ${buttonStyle("HRMS")}`}
+                tabIndex={0}
+              >
+                HRMS
+              </button>
+              <button
+                onClick={() => handleButtonClick("CareerCounselling")}
+                className={`py-2 px-4 rounded-full ${buttonStyle("CareerCounselling")}`}
+                tabIndex={0}
+              >
+                Career Counselling
+              </button>
           </div>
         </div>
       </div>
-      <div className="z-10 self-center -mt-[300px] ml-4 mr-6 w-full max-w-full max-md:mt-0 max-md:max-w-full">
+      <div className="z-10 self-center -mt-[244px] ml-4 mr-6 w-full max-w-full max-md:mt-0 max-md:max-w-full">
         <div className="flex gap-10 justify-center  items-center w-full min-h-screen">
-          {pricingData?.map((plan, index) => (
-            <PricingCard key={index} {...plan} />
-          ))}
+        {pricingData[selectedCategory]?.map((plan, index) => (
+              <PricingCard key={index} {...plan} />
+            ))}  
         </div>
       </div>
     </div>
