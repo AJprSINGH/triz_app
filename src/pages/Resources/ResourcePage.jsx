9a6f3d29216cbfd5cce6265877457b4478189c9f
Ajit Singh Parihar <ajitsinghprhr123@gmail.com>
2025-01-01 15:02:45 +0530

    Check 94

--- src/pages/Resources/ResourcePage.jsx
+++ src/pages/Resources/ResourcePage.jsx
@@ -25,14 +25,14 @@ function ResourcePage() {
         alt="Resource page header"
         className="object-contain w-full aspect-[2.6] max-md:max-w-full"
       />
-      <section className="flex z-6 flex-col self-center mt-12 sm:mx-2  font-bold  w-full max-w-full max-md:mt-4 max-md:max-w-full">
+      <section className="flex flex-wrap mt-12 items-center sm:mx-2 lg:mx-0">
         <div className="flex flex-wrap gap-3 justify-center items-center w-full text-lg text-white max-md:gap-2 max-md:max-w-full">
           {resourceTypes.map((type, index) => (
             <ResourceButton key={index} text={type} className="lg:px-3 lg:py-2 sm:px-1.5 sm:py-1 bg-blue-600 rounded-md lg:text-sm sm:text-xs max-md:text-xs" />
           ))}
         </div>
-        <div className="flex overflow-hidden flex-col px-12 py-4 mt-24 lg:ml-40 sm:mx-20 sm:items-center max-w-full text-black whitespace-nowrap rounded-2xl border-sky-500 border-solid border-[5px] lg:w-[310px] sm:w-[310px] max-md:px-5 max-md:mt-10">
-                <img loading="lazy" src="/resources/Group 125.png" alt="Group 125" className="w-full h-auto object-contain" />
+        <div className="flex overflow-hidden flex-col lg:px-12 lg:py-4 sm:px-6 sm:py-2 lg:mt-24 sm:mt-20 sm:mx-10 lg:ml-40 sm:items-center max-w-full text-black whitespace-nowrap rounded-2xl border-sky-500 lg:border-solid lg:border-[5px] sm:border-solid sm:border-[2px] lg:w-[310px] sm:w-[90%] max-md:px-5 max-md:mt-10">
+                <img loading="lazy" src="/resources/Group 125.png" alt="Group 125" className="lg:w-full sm:w-[200px] h-auto object-contain" />
         </div>
         {/* Contact Us Section */}
         <div className="contact-us w-full mt-16">
